dc34608254a03e824442cf05eaee587ff549e824
ThePremkumar <spremkumar2424@gmail.com>
2025-12-18 17:17:33 +0530

    fix the error

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const app = express();
 app.use(bodyParser.json());
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+  });
 
 // Routes
 app.use('/api/auth', authRoutes);
